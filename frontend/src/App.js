af6e5bacf51f7f278d826f9021493c694e80eea4
Moazam05 <salmanmoazam08@gmail.com>
2022-11-01 14:44:09 +0500

    Dependencies install and routes r setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+// React Imports
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// Custom Imports
+import Login from './views/Login';
+import Home from './views/Home';
+import PageNotFound from './views/Pages/PageNotFound';
 
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/' element={<Home />} />
+      {/* 404 Page */}
+      <Route path='*' element={<PageNotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ frontend/src/views/Pages/PageNotFound.js
@@ -0,0 +1,34 @@
+// React Imports
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className='h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]'>
+      <h1 className='text-9xl font-extrabold text-white tracking-widest'>
+        404
+      </h1>
+      <div className='bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute'>
+        Page Not Found
+      </div>
+      <div className='mt-5'>
+        <button
+          className='relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring'
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          <span className='absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0'></span>
+
+          <span className='relative block px-8 py-3 bg-[#1A2238] border border-current'>
+            <div>Go Home</div>
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
