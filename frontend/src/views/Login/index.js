1b41ba8e8e4cdb68c722d3d6b4b49a065e9eed2e
Moazam05 <salmanmoazam08@gmail.com>
2022-11-01 18:48:37 +0500

    google authentication functionality

--- frontend/src/views/Login/index.js
+++ frontend/src/views/Login/index.js
@@ -1,15 +1,54 @@
 // React Imports
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 // React Google Imports
 import GoogleLogin from 'react-google-login';
+import { gapi } from 'gapi-script';
 // React Icons
 import { FcGoogle } from 'react-icons/fc';
 // Images Imports
 import logo from '../../assets/logowhite.png';
 import shareVideo from '../../assets/share.mp4';
+// Custom Imports
+import { client } from '../../client';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: process.env.REACT_APP_GOOGLE_API_TOKEN,
+        scope: 'email',
+      });
+    }
+
+    gapi.load('client:auth2', start);
+  }, []);
+
+  const onSuccess = function (response) {
+    console.log('SUCCESS', response);
+
+    localStorage.setItem('user', JSON.stringify(response.profileObj));
+
+    const { name, googleId, imageUrl } = response?.profileObj;
+
+    const doc = {
+      _id: googleId,
+      _type: 'user',
+      userName: name,
+      image: imageUrl,
+    };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate('/', { replace: true });
+    });
+  };
+
+  const onFailure = function (response) {
+    console.log('FAILED', response);
+  };
+
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
       <div className='relative w-full h-full'>
@@ -26,6 +65,25 @@ const Login = () => {
         <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
           <div className='p-5'>
             <img src={logo} alt='logo' width='130px' />
+          </div>
+
+          <div className='shadow-2xl'>
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              render={(renderProps) => (
+                <button
+                  type='button'
+                  className='bg-mainColor flex justify-center items-center p-3 rounded-lg outline-none'
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className='mr-4' /> Sign in with Google
+                </button>
+              )}
+              onSuccess={onSuccess}
+              onFailure={onFailure}
+              cookiePolicy='single_host_origin'
+            />
           </div>
         </div>
       </div>
